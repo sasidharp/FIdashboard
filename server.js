86bcfd7b0da7937f8fff637a3f186952be4a837c
sasidharp <sasidharp@gmail.com>
2018-08-31 01:45:23 +0530

    including server file

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+// Create link to Angular build directory
+
+var distDir = __dirname + "/dist/FIdashboard";
+console.log(distDir);
+app.use(express.static(distDir));
+
+var server = app.listen( 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
