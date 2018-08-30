cccddec0ab3ebad06ca4229dd1a67e61d2faeb78
sasidharp <sasidharp@gmail.com>
2018-08-31 01:50:14 +0530

    add package.json

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var distDir = __dirname + "/dist/FIdashboard";
 console.log(distDir);
 app.use(express.static(distDir));
 
-var server = app.listen( 8080, function () {
+var server = app.listen( process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
