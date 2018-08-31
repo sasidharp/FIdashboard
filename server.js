0b9b44fe41051cde224e57fd23b4c6539badc0ca
sasidharp <sasidharp@gmail.com>
2018-08-31 17:32:23 +0530

    process invo

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const app = express()
 
 // Create link to Angular build directory
 
-var distDir = __dirname + "/dist/FIdashboard";
-console.log(distDir);
-app.use(express.static(distDir));
+// var distDir = __dirname + "/dist/FIdashboard";
+// console.log(distDir);
+// app.use(express.static(distDir));
 
-var server = app.listen( process.env.PORT || 8080, function () {
+var server = app.listen( process.env.PORT , function (req,res) {
     var port = server.address().port;
+    app.get('/', function(req, res){
+      res.send({'hello':'world'});
+    })
     console.log("App now running on port", port);
   });
+  
