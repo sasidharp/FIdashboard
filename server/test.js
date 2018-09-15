5d7ec6d3dc045d995ac8271860d1df4887e10290
sasidharp <sasidharp@gmail.com>
2018-09-16 00:29:38 +0530

    AAD MSI

--- server/test.js
+++ server/test.js
@@ -1,6 +1,6 @@
-const rp = require('request');
+const request = require('request');
 token = '';
-resource = 'https://fcmd.vault.azure.net/secrets/mongo/4ab3b721223c4e12b9ce6bedb01a061c';
+resource = 'https://vault.azure.net';
 apiver = '2017-09-01';
 var options = {
   method: 'GET',
@@ -12,8 +12,18 @@ var options = {
 // Call ADL
 console.log('Executing Request to Fetch AAD token');
 request(options, function (error, response, body) {
-    console.log(error);
-    console.log(response);
-    console.body(body)
-    
+  console.log('calling key valut');
+  body = JSON.parse(body);
+  token = 'Bearer ' + body.access_token;
+  console.log(token);
+  var options = {
+    method: 'GET',
+    url: `https://fcmd.vault.azure.net/secrets/sendgridkey/c43bc362ef6043d8a0aafe8361d497e8?api-version=2016-10-01`,
+    headers: {
+      'Authorization': token
+    }
+  };
+  request(options, function (error, response, body) {
+    console.log(JSON.parse(body));
+  });
 });
