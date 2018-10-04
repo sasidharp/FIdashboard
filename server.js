b0064c02c5a1d55dcc877bf662a578f4a9c94a02
Sasidhar Puranam <sapurana@microsoft.com>
2018-10-04 15:12:21 +0530

    final server

--- server.js
+++ server.js
@@ -58,23 +58,45 @@ app.get('/fijobs', function (err, res) {
         });
     })
 });
+//fetch the latest data .
 app.get('/hrjobs', function (err, res) {
-    /*  This code will get the entries from SAP and pushes into Mongo DB*/
-    //Use connect method to connect to the server
-    MongoClient.connect(url, function (err, client) {
-        if (err != null) { return 'Could not connect. Exiting' };
-        //Get the database name    
-        const db = client.db('sapjobs');
-        //Get the collections 'items' from database 'sapjobs'    
-        const collection = db.collection('hritems');
-        //Insert data
-        collection.find({}).sort({ _id: -1 }).limit(1).toArray(function (err, results) {
-            res.send(results);
-            client.close();
-        });
 
-    });
-})
+    var options = {
+        method: 'GET', url: `${process.env["MSI_ENDPOINT"]}/?resource=${MSI_KEY_VAULTURL}&api-version=${API_VER}`,
+        headers: { 'Secret': process.env["MSI_SECRET"] }
+    };
+
+    request(options, function (error, response, body) {
+        authToken = ('Bearer ' + JSON.parse(body).access_token);
+        var options = {
+            method: 'GET', url: `https://fcmd.vault.azure.net/secrets/mongo/4ab3b721223c4e12b9ce6bedb01a061c?api-version=2016-10-01`,
+            headers: { 'Authorization': authToken }
+        };
+        request(options, function (error, response, body) {
+            if (response.statusCode === 200) {
+                dbConn = JSON.parse(body).value;
+                /*  This code will get the entries from SAP and pushes into Mongo DB*/
+                //Use connect method to connect to the server
+                MongoClient.connect(dbConn, function (err, client) {
+                    if (err != null) { return 'Could not connect. Exiting' };
+                    //Get the database name    
+                    const db = client.db('sapjobs');
+                    //Get the collections 'items' from database 'sapjobs'    
+                    const collection = db.collection('hritems');
+                    //Insert data
+                    collection.find({}).sort({ _id: -1 }).limit(1).toArray(function (err, results) {
+                        res.send(results);
+                        client.close();
+                    });
+                });
+            } else {
+                res.send(500, 'Internal error. Problem with MongoString');
+                console.log('No permission to view Secret');
+                return;
+            }
+        });
+    })
+});
 // redo
 app.get('/*', function (req, res) {
     console.log(distDir);
